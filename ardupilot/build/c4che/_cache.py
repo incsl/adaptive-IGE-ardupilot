8d96f0c4019fe4dbc87761af10c07731f632ba3d
Hoang Anh Tran <tranhoanganh1512@gmail.com>
2021-10-21 16:01:41 +0900

    First commit

---
+++ ardupilot/build/c4che/_cache.py
@@ -0,0 +1,6 @@
+AUTOCONFIG = True
+BINDIR = '/usr/bin'
+BOARD = 'CUAVv5'
+DEBUG = False
+LIBDIR = '/usr/lib'
+PREFIX = '/usr'
---
+++ ardupilot/build/sitl/libraries/AP_GPS/RTCM3_Parser.cpp.0.d
@@ -0,0 +1,3 @@
+libraries/AP_GPS/RTCM3_Parser.cpp.0.o: \
+ ../../libraries/AP_GPS/RTCM3_Parser.cpp ap_config.h \
+ ../../libraries/AP_GPS/RTCM3_Parser.h
